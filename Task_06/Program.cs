b58e4f4c0a48daf02a153ea669fe888c6ef2127b
darlyncarmenate <darlyncarmenate@gmail.com>
2022-04-25 11:23:52 +0200

    Четсные числа от 1 до N

---
+++ Task_06/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+string a = Console.ReadLine();
+int N = int.Parse(a);
+
+int i = 0;
+while (i <= N)
+{
+i = i + 2;
+Console.Write($"{i} ");
+}
