1a93601c931a33f4a26a911b59f78515b7bbe9e2
darlyncarmenate <darlyncarmenate@gmail.com>
2022-04-25 10:55:25 +0200

    Обратное значение числа

---
+++ Task_02/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая на вход принимает значение, а на выходе показывает обратное значение.
+
+Console.WriteLine("Введите число: ");
+string n = Console.ReadLine();
+int number = int.Parse(n);
+
+Console.WriteLine((double) 1 / number);
