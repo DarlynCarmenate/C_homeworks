45be755d573def346cc3473fbe0ae24a067404b8
darlyncarmenate <darlyncarmenate@gmail.com>
2022-04-25 10:54:45 +0200

    Модуль числа

---
+++ Task_01/Program.cs
@@ -0,0 +1,10 @@
+﻿//  Напишите программу вычисления модуля числа
+
+Console.WriteLine("Введите число: ");
+string n = Console.ReadLine();
+int number = int.Parse(n);
+
+if (number < 0) number *= -1;
+
+Console.WriteLine($"Модуль числа: {number}");
+
