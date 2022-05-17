83a70aa15618dd0d1ddc9f1d424ccf5a953a29da
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-17 22:42:53 +0200

    Вторая цифра трехзначного числа

---
+++ 2Task_07/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая на вход принимает трехзнаное число, а на выходе
+// показывает вторую цифру этого числа
+
+int Prompt(string message){
+    Console.Write(message);
+    string input = Console.ReadLine();
+    int value = int.Parse(input);
+    return value;
+}
+
+void SecondNum(int number)
+{
+    if (number < 99 || number > 999)
+    {
+        Console.WriteLine("Wrong digit");
+    }
+    else
+    {
+    Console.WriteLine($"The second number is: {number/10%10}");
+    }
+}
+
+int num = Prompt("Please, enter triple digits: ");
+SecondNum(num);
