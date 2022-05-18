3586b174287157fe62ac8dc1480da8ec2c17aa0e
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-18 22:15:20 +0200

    День недели по цифре

---
+++ 2Task_10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+void WeekDay()
+{
+    int day = new Random().Next(1, 8);
+    Console.Write($"{day}: ");
+    if (day == 6 || day == 7)
+    {
+        Console.WriteLine("It's a holiday");
+    }
+    else
+    {
+        Console.WriteLine("It's a workday");
+    }
+}
+
+WeekDay();
