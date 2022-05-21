424cfb6a583459997bcbd13d137fe1607a82558a
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-21 06:15:58 +0200

    Таблица кубов от 1 до N

---
+++ 3Task_13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+void Squares(int num)
+{
+    double[] arr = new double[num];
+    int i = 0;
+    while (i < num)
+    {
+    arr[i] = Math.Pow((i+1), 3);
+    Console.Write($"{arr[i]}, ");    
+    i++;
+    }
+    Console.WriteLine();
+}
+
+Squares(5);
+Squares(7);
+Squares(9);
