65f4d94cc75d9d4c39583950bf611f8e820cc2c1
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-21 05:44:51 +0200

    Полиндром

---
+++ 3Task_11/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+
+Console.WriteLine(Polyndrom("Enter a number"));
+
+string Polyndrom(string message)
+{
+    Console.WriteLine(message);
+    string number = Console.ReadLine();
+    int len = number.Length;
+
+    int i = 0;
+    string answer = "Polyndrom";
+
+    while (i < len / 2)
+    {
+        if (number[i] != number[len - i - 1])
+        {
+            answer = "Not polyndrom";
+            break;
+        }
+        else if (number[i] == number[len - i - 1])
+        {
+            i++;
+        }
+    }
+    return answer;
+}
+
