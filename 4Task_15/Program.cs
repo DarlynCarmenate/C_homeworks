5dc4235bc740afe14fd35a8294c86232bf156528
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-24 23:17:01 +0200

    Сумма цифр в числе

---
+++ 4Task_15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int DigitSum(int number)
+{
+    int result = 0;
+    while(number > 0)
+    {
+        result = result + number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+
+Console.WriteLine(DigitSum(452));
+Console.WriteLine(DigitSum(82));
+Console.WriteLine(DigitSum(9012));
