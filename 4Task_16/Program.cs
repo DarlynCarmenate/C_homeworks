84381747c5fba31fb42f94f644cf235a9a009238
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-24 23:30:54 +0200

    Функции для создания и вывода массива

---
+++ 4Task_16/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов случайными числами 
+// и выводит их на экран. Оформите заполнение массива и вывод в виде функции 
+
+int[] RandArray (int length)
+{
+	int[] answer = new int[length];
+	for(int i = 0; i < length; i++)
+	{
+		answer[i] = new Random().Next(1, 100);
+	}
+	return answer;
+}
+
+void PrintArray(int[] arr)
+{
+	int count = arr.Length;
+    Console.Write("[");
+	for(int pos = 0; pos < count - 1; pos++)
+	{
+		Console.Write($"{arr[pos]}, ");
+	}
+    Console.Write($"{arr[arr.Length - 1]}]");
+}
+
+int[] array = RandArray(8);
+PrintArray(array);
