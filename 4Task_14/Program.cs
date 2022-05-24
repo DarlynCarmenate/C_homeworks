804132fc9b87a4480ffe3d57193fbad916f6dbbc
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-24 23:09:28 +0200

    Число А в степени B

---
+++ 4Task_14/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите цикл, который принимает на вход 
+// два числа (A и B) и возводит число A в натуральную степень B.
+
+int DigitPow(int a, int b)
+{
+    int result = a;
+    for (int i = 1; i < b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+Console.WriteLine(DigitPow(3, 5));
