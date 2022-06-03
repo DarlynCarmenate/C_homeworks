26dd2a61db2d2cdbcb46d2d9dea59bad1666a10c
darlyncarmenate <darlyncarmenate@gmail.com>
2022-06-04 01:09:01 +0200

    Числа больше 0, вариант преподавателя

---
+++ 6Task_22_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 
+// ввёл пользователь.
+
+int PosNum()
+{
+    string word;
+    int count = 0;
+
+    while (true)
+    {
+        Console.WriteLine("Enter a number: ");
+        word = Console.ReadLine();
+        if (word == "") return count;
+        else if (int.Parse(word) > 0) count++;
+    }
+}
+
+Console.WriteLine(PosNum());
