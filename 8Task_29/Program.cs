71ab8ddbfaf6f593e0d11edf0f496b43f5d5722d
darlyncarmenate <darlyncarmenate@gmail.com>
2022-06-08 01:01:18 +0200

    Сортировка строк в матрице

---
+++ 8Task_29/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте двумерный массив. Напишите программу, которая
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] FillArray(int row, int column, int from, int to)
+{
+    int[,] array = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(from, to);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SortRows(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < arr.GetLength(1); k++)            
+            {
+                if(arr[i, j] < arr[i, k])
+                (arr[i, j], arr[i, k]) = (arr[i, k], arr[i, j]);
+            }
+        }
+    } 
+    return arr;
+}
+
+int[,] array = FillArray(5,5, -10, 10);
+PrintArray(array);
+SortRows(array);
+Console.WriteLine();
+PrintArray(array);
