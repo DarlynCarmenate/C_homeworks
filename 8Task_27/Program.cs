22f8d308c9b51db4ce1bdf42db314d884f0b620a
darlyncarmenate <darlyncarmenate@gmail.com>
2022-06-09 17:58:45 +0200

    Частотный словарь

---
+++ 8Task_27/Program.cs
@@ -0,0 +1,55 @@
+﻿// Составить частотный словарь элементов двумерного массива. Частотный словарь содержит 
+// информацию о том, сколько раз встречается элемент входных данных. 
+// Значения элементов массива 0..9
+
+int[,] FillArray(int row, int column, int from, int to)
+{
+    int[,] array = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(from, to);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] FreqDictionary(int[,] arr)
+{
+    int[] freq = new int[10];
+
+    foreach (int item in arr) freq[item] += 1;
+    return freq;
+}
+
+void PrintMass(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.WriteLine($"{i} meets: {arr[i]}");
+    Console.WriteLine();
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = FillArray(row, column, 0, 10);
+PrintArray(arr_1);
+
+int[] mass = FreqDictionary(arr_1);
+PrintMass(mass);
