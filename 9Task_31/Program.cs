be20896124e3bd02281c51de4f39c7c993e88630
darlyncarmenate <darlyncarmenate@gmail.com>
2022-06-11 15:23:03 +0200

    Числа от М до N - рекурсия

---
+++ 9Task_31/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные 
+// числа в промежутке от M до N с помощью рекурсии.
+
+void OddNums(int m, int n)
+{
+    if (n < m) return;
+    if(n % 2 == 1) n -= 1;
+    OddNums(m, n - 2);
+    Console.Write($"{n} ");
+}
+
+OddNums(1, 11);
+Console.WriteLine();
+OddNums(10, 20);
